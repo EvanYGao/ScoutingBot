7d83f7efc02957f5d774ca593ec7e4c42d124766
Bryan Li <bryanyuezhouli@gmail.com>
2019-11-22 16:36:27 -0600

    changed config directory

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -14,15 +14,35 @@ firebase = pyrebase.initialize_app(config)
 db = firebase.database()
 # print(db.child('devices').get().val())
 
-def get_matches(event: str, team : int):
-  team = str(team) if len(str(team)) == 4 else ' ' + str(team)
+def get_matches(event: str, team: int) -> list:
+  """
+  A function that queries the database for the match data of a team for a specific event or all events
+
+  :param event: The event ID of the event in the Firebase database. use 'all' for all events in the year.
+  :param team: The team number of the team that you want to query.
+
+  :returns: A list of all the match data a team has for the specified event. Returns None if no data
+  """
+  # The database has a space before team numbers less than 4 in length
+  team = f'{team:04d}'.replace('0', ' ')
   matches = list()
-  for x in range(1, 150):
-    data = db.child('match-data').child(event).child(f'{x:03d}-{team}').get().val()
-    if data != None:
-      matches.append(data)
+  if event == 'all':
+    events = list()
+    for key, value in db.child('match-data').get().val():
+      events.append[key]
+      print(events)
+  else:
+    for x in range(1, 150):
+      data = db.child('match-data').child(event).child(f'{x:03d}-{team}').get().val()
+      if data != None:
+        matches.append(data)
   return matches
 
-for x in get_matches('gal', 5414):
-  print(x['final_comment'])
+def get_raw_percents(event: str, team: int):
+  pass
+
+# for x in get_matches('gal', 5414):
+#   print(x['final_comment'])
 # print(db.child('match-data').child('ftcmp').child('012-5414').get().val())
+get_matches('all', 5414)
+get_matches()
