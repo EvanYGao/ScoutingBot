42291b3ecaa98eba4583bdafdd0f9478897b93bb
Bryan Li <bryanyuezhouli@gmail.com>
2019-11-22 17:01:25 -0600

    Added 'all' support for get_matches
    Also added docstring for get_matches

--- firebase/firebase.py
+++ firebase/firebase.py
@@ -16,24 +16,27 @@ db = firebase.database()
 
 def get_matches(event: str, team: int) -> list:
   """
-  A function that queries the database for the match data of a team for a specific event or all events
+  A function that queries the database for the match data of a team for a specific event or all 
+  events
 
-  :param event: The event ID of the event in the Firebase database. use 'all' for all events in the year.
+  :param event: The event ID of the event in the Firebase database. Use 'all' for all events in
+  the year.
   :param team: The team number of the team that you want to query.
 
-  :returns: A list of all the match data a team has for the specified event. Returns None if no data
+  :returns: A list of all the match data a team has for the specified event.
   """
   # The database has a space before team numbers less than 4 in length
   team = f'{team:04d}'.replace('0', ' ')
   matches = list()
   if event == 'all':
-    events = list()
-    for key, value in db.child('match-data').get().val():
-      events.append[key]
-      print(events)
+    events = list(db.child('match-data').shallow().get().val())
+    for event in events:
+      for match in get_matches(event, int(team)):
+        matches.append(match)
   else:
-    for x in range(1, 150):
-      data = db.child('match-data').child(event).child(f'{x:03d}-{team}').get().val()
+    # We can realistically expect less than 150 matches per event
+    for match in range(1, 150):
+      data = db.child('match-data').child(event).child(f'{match:03d}-{team}').get().val()
       if data != None:
         matches.append(data)
   return matches
@@ -44,5 +47,4 @@ def get_raw_percents(event: str, team: int):
 # for x in get_matches('gal', 5414):
 #   print(x['final_comment'])
 # print(db.child('match-data').child('ftcmp').child('012-5414').get().val())
-get_matches('all', 5414)
-get_matches()
+print(get_matches('all', 5414))
